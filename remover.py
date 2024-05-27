acd0ac99b295edac7cf4d0bda73fd012521c0d9a
Baptiste TAVERNE <baptiste.taverne@polytechnique.edu>
2024-05-27 09:56:27 -0400

    Remover

--- remover.py
+++ remover.py
@@ -1,12 +1,26 @@
+# This script is designed to force remove a directory and all its contents. 
+# It is handy to use if teh downloded dataset refuses to  be removed because of git annex restrictions.
+# /!\ Be careful, this script will remove the directory and all its contents permanently.
+
+
 import os
 import subprocess
+import argparse
+
+parser = argparse.ArgumentParser(description='Remove the data directory.')
+parser.add_argument('--directory_to_remove', type=str, default=None,
+                    help='the path to the data directory, None by Default')
+
+args = parser.parse_args()
+directory_to_remove = args.directory_to_remove
+
 
 def remove_directory(path):
     for root, dirs, files in os.walk(path):
         for file in files:
-            os.chmod(os.path.join(root, file), 0o750)  # Give read, write, and execute permissions
+            os.chmod(os.path.join(root, file), 0o600)  # Give read and write permissions to the current user only
         for dir in dirs:
-            os.chmod(os.path.join(root, dir), 0o750)  # Give read, write, and execute permissions
+            os.chmod(os.path.join(root, dir), 0o600)  # Give read and write permissions to the current user only
     subprocess.run(f"rm -r {path}", shell=True)
 
-remove_directory("/home/ge.polymtl.ca/p120530/Neuro_Poly_Training/3D_multi_contrast/Bidsification/data")
+remove_directory(directory_to_remove)
